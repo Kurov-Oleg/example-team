b320a1aed2906ad0e032eae1d19b5abe65e30d40
z <ee>
2021-10-28 11:11:47 +0300

    wrr

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -8,6 +8,6 @@ public class Welcomer{
 		return "Farewell, good hunter. May you find your worth in waking world.";
 	}
 	public String saySomething() {
-	    return "go your road, hunter"
+	    return "go your road, hunter";
 	}
 }
